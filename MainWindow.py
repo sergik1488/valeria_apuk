3a46159d24beef4c9868e0cc8dfd0bd903f5b9ff
Чернов Даниил <dvchernov@miem.hse.ru>
2022-06-10 18:43:01 +0300

    update

--- MainWindow.py
+++ MainWindow.py
@@ -3,26 +3,47 @@ import pandas as pd
 import numpy as np
 import os.path
 import sys
+from PIL import Image, ImageTk
 
+massstart = []
 
 
 def start(root):
+    def opennextwindov():
+        for i in massstart:
+            if i.winfo_viewable():
+                i.grid_remove()
+            else:
+                i.grid()
+        MainWind(root)
+
+    root["bg"] = "SkyBlue1"
     lbl1 = tki.Label(
         root,
-        text="Нажмите старт для работы с БВ",
+        text="Нажмите старт для работы с БД",
         font=(
             'Times',
             24,
-            'bold'))
-    lbl1.grid(column=1, row=0, pady=100, padx=80)
+            'bold'),
+        bg='SkyBlue1')
+    lbl1.grid(column=1, row=0, pady=100, padx=400)
     btn = tki.Button(
         root,
         text='Старт',
         font=(
-            'Colibry',
+            'Times',
             14,
             'bold'),
-        bg='white',
-        fg='black')
+        background='cyan1',
+        fg='black',
+        command=opennextwindov)
     btn.grid(column=1, row=1)
-    mass = [lbl1, btn]
+    massstart = [lbl1, btn]
+
+
+def MainWind(root):
+    tabl1 = None
+    tabl2 = None
+    tabl3 = None
+    tabl4 = None
+
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import tkinter as tki
+from tkinter import *
 import pandas as pd
 import numpy as np
 import os.path
 import sys
 import MainWindow as mw
+from PIL import ImageTk, Image
+
 
 def Main():
     root = tki.Tk()
+
     root.geometry("1280x720")
     root.title("Project Sem")
     mw.start(root)
